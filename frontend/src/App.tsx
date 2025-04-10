1bd241ed518123af8e0dc3b8f8135c4632dcf49c
Shabbir-012 <shabbirahmedfaysal@gmail.com>
2025-04-10 11:02:29 +0600

    initial setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Hello new World</h1>
+      <Button variant={"destructive"}>Click me</Button>
+    </>
+  )
+}
+
+export default App
