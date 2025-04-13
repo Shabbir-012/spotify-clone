a1784ea75618d3639110414a7372b43599155e42
Shabbir-012 <shabbirahmedfaysal@gmail.com>
2025-04-13 18:02:16 +0600

    some update

--- backend/src/routes/stats.route.js
+++ backend/src/routes/stats.route.js
@@ -1,10 +1,42 @@
 import { Router } from "express";
-
+import { Song } from "../models/song.model.js";
+import { User } from "../models/user.model.js";
+import { Album } from "../models/album.model.js";
 
 const router = Router();
 
-router.get("/", (req,res) => {
-    res.send("stats route with GET Method");
-})
+router.get("/", async (req, res, next) => {
+  try {
+    // const totalSongs = await Song.countDocuments();
+    // const totalUsers = await User.countDocuments();
+    // const totalAlbums = await Album.countDocuments();
+
+    //Promise
+    const { totalSongs, totalUsers, totalAlbums } = await Promise.all([
+      Song.countDocuments(),
+      User.countDocuments(),
+      Album.countDocuments(),
+
+      Song.aggregate([
+        {
+          $unionWith: {
+            coll: "albums",
+            pipeline: [],
+          },
+        },
+        {
+          $group: {
+            _id: "$artist",
+          },
+        },
+        {
+          $count: "count",
+        },
+      ]),
+    ]);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default router;
